148605e939f30ea75254b16455f87478d33a2653
Jeffrey Li <jeffrey.li@cantab.net>
2017-02-23 11:23:18 +0800

    completed Wednesday

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,50 +1,46 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
-    <h3>One of my favorite bands is: {{favoriteBand}}</h3>
-    <p>If I had to choose a favorite painter it would
-    be: {{favoritePainter}}</p>
-    <p>The number of slices of pie I would like is:
-    {{sliceOfPie}}</p>
-    <h3>One of my favorite albums is: </h3>
-    <p>{{favoriteAlbum.title}}</p>
-    <p>By {{favoriteAlbum.artist}}</p>
-    <p>Released in {{favoriteAlbum.released}}</p>
-    <h3>Here are my favorite pies!</h3>
-    <div class="pie" *ngFor="let  currentPie of favoritePies">
-      <p>{{currentPie}}</p>
-    </div>
-    <h3>Here are my favorite albums!</h3>
-    <div class="album" *ngFor="let album of albums">
-      <p>{{album.title}}</p>
-      <p>By {{album.artist}}</p>
-      <p>Released in {{album.released}}</p>
-    </div>
+    <pies></pies>
+
+    <task-list
+      [childTaskList]="masterTaskList"
+      (clickSender)="showDetails($event)"
+     ></task-list>
+
+    <edit-task
+      [childSelectedTask]="selectedTask"
+      (doneClickedSender)="finishedEditing()"
+    ></edit-task>
+
+    <new-task
+      (newTaskSender)="addTask($event)"
+    ></new-task>
+    
   </div>
   `
 })
 
 export class AppComponent {
-  favoriteBand: string = 'Ani DiFranco';
-  favoritePainter: string = 'Van Gogh';
-  sliceOfPie: number = 3;
-  favoriteAlbum : Album = new Album("Disintegration","The Cure", 1989);
-  favoritePies: string [] = ["Apple","Banana Cream","Blackberry"];
-  albums: Album []=[
-    new Album("Pulse","Pink Floyd", 1995),
-    new Album("Funhouse","The Stooges", 1970),
-    new Album("Twilight of the Thunder God", "Amon Amarth", 2008),
-    new Album("Dilate", "Ani DiFranco", 1996),
-    new Album("Chopin - Complete Nocturnes", "Brigitte Engerer", 2015),
-    new Album("Axis Bold As Love", "The Jimi Hendrix Experience", 1967)
+  public masterTaskList: Task[] = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
   ];
+  selectedTask: Task = null;
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
   }
-
-
-export class Album {
-  constructor (public title: string, public artist: string, public released: number){}
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
+  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,30 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule}     from '@angular/forms';
+import { PiesListComponent} from './pies-list.component';
+import { TaskListComponent} from './task-list.component';
+import { EditTaskComponent} from './edit-task.component'
 import { AppComponent }   from './app.component';
+import { NewTaskComponent} from './new-task.component';
+import { CompletenessPipe} from './completeness.pipe';
+import { TaskComponent } from './task.component'
+
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule
+],
+  declarations: [
+    PiesListComponent,
+    TaskListComponent,
+    AppComponent,
+    EditTaskComponent,
+    NewTaskComponent,
+    CompletenessPipe,
+    TaskComponent,
+  ],
+  bootstrap: [AppComponent]
 })
 
 export class AppModule { }
